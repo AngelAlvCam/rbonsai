d51ecee0f062f51263a8b4c39b4db081f52a26f2
AngelAlvCam <fluttershady@gmail.com>
2025-05-15 11:43:11 -0600

    rename variables for clarity

--- rbonsai.R
+++ rbonsai.R
@@ -13,34 +13,34 @@ bonsai_text <- bonsai_text |>
 dims <- system("stty size", intern = TRUE) |>
     str_split(" ")
 dims <- as.integer(dims[[1]])
-t_rows <- dims[1]
-t_cols <- dims[2]
+window_height <- dims[1]
+window_width <- dims[2]
 
 # Split bonsai in lines
 bonsai_length <- str_length(bonsai_text)
-starts <- seq(1, bonsai_length, t_cols)
-ends <- seq(t_cols, bonsai_length, t_cols)
+starts <- seq(1, bonsai_length, window_width)
+ends <- seq(window_width, bonsai_length, window_width)
 bonsai_lines <- bonsai_text |>
     substring(starts, ends)
 
 # Identify top padding
-max_row <- max(which(str_detect(bonsai_lines, regex("^\\s+$"))))
-if (!is.finite(max_row)) {
-    max_row <- 1
+top_row <- max(which(str_detect(bonsai_lines, regex("^\\s+$"))))
+if (!is.finite(top_row)) {
+    top_row <- 1
 }
 
 # Identify left and right padding
 list <- str_locate_all(bonsai_lines, regex("[^' ']"))
-min_index = t_cols
-max_index = 1
-for (i in seq(1, t_rows)) {
+left_col = window_width 
+right_col = 1
+for (i in seq(1, window_height)) {
     if (length(list[[i]] > 0)) {
         print(list[[i]][1,1])
-        min_index <- min(min_index, list[[i]][1, 1])
-        max_index <- max(max_index, list[[i]][nrow(list[[i]]), 1])
+        left_col <- min(left_col, list[[i]][1, 1])
+        right_col <- max(right_col, list[[i]][nrow(list[[i]]), 1])
     }
 }
 
 # Remove padding from `bonsai_lines`
-bonsai_lines <- bonsai_lines[seq(max_row + 1, t_rows)] |>
-    substring(min_index, max_index)
+bonsai_lines <- bonsai_lines[seq(top_row + 1, window_height)] |>
+    substring(left_col, right_col)
